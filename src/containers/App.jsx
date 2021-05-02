6357ee5d3d474376ef3a213efce8529d6b55ca09
Johnnie Lopez <johesloc@gmail.com>
2021-05-02 17:47:42 -0500

    add useState and useEffect to use API with fetch

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -8,37 +8,48 @@ import Footer from '../components/Footer'
 
 import '../assets/style/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    const [videos, setVideos] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+            .then(response => response.json())
+            .then(data => setVideos(data))
+    },[])
 
-        <Categories title='Mi lista'>
-            <Carrousel>
-                <CarrouselItem />
-                <CarrouselItem />
-                <CarrouselItem />
-                <CarrouselItem />
-            </Carrousel>
-        </Categories>
+    console.log(videos)
 
-        <Categories title='Tendencias'>
-            <Carrousel>
-                <CarrouselItem />
-                <CarrouselItem />
-            </Carrousel>
-        </Categories>
+    return(
+        <div className="App">
+            <Header />
+            <Search />
 
-        <Categories title='Originales de Platzi Videos' >
-            <Carrousel>
-                <CarrouselItem />
-                <CarrouselItem />
-                <CarrouselItem />
-            </Carrousel>
-        </Categories>
-        
-        
-        <Footer />
-    </div>
+            <Categories title='Mi lista'>
+                <Carrousel>
+                    <CarrouselItem />
+                    <CarrouselItem />
+                    <CarrouselItem />
+                    <CarrouselItem />
+                </Carrousel>
+            </Categories>
+
+            <Categories title='Tendencias'>
+                <Carrousel>
+                    <CarrouselItem />
+                    <CarrouselItem />
+                </Carrousel>
+            </Categories>
+
+            <Categories title='Originales de Platzi Videos' >
+                <Carrousel>
+                    <CarrouselItem />
+                    <CarrouselItem />
+                    <CarrouselItem />
+                </Carrousel>
+            </Categories>
+            
+            
+            <Footer />
+        </div>
 )
+}
 export default App
